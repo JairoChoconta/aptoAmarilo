115a391da47488b4765b23c3eb982eb5806731e8
JairoChoconta <jalexanderchoconta97@gmail.com>
2021-07-06 11:42:10 -0500

    cambios agregué nuevas funciones

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
-/*==================== SHOW MENU ==================*/
+/*==================== SHOW MENU ====================*/
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
-    
+
     // Validate that variables exist
     if(toggle && nav){
         toggle.addEventListener('click', () =>{
@@ -11,9 +11,10 @@ const showMenu = (toggleId, navId) =>{
         })
     }
 }
+
 showMenu('nav-toggle','nav-menu')
 
-/*==================== SWIPER JS ==================*/
+/*==================== SWIPER JS ====================*/
 let galleryThumbs = new Swiper('.gallery-thumbs', {
     spaceBetween: 0,
     slidesPerView: 0,
@@ -28,14 +29,58 @@ let galleryTop = new Swiper('.gallery-top', {
     }
 })
 
-/*==================== POPUP ==================*/
+/*====================  INFO MODAL ====================*/
+const modalViews = document.querySelectorAll('.info__modal'),
+    modalBtns = document.querySelectorAll('.info__button'),
+    modalBtnsNav = document.querySelectorAll('.info__button-nav')
+    modalCloses = document.querySelectorAll('.info__modal-close')
+
+let modal = function(modalClick) {
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () =>{
+        modal(i)
+    })
+})
+
+modalBtnsNav.forEach((modalBtnNav, j) => {
+    modalBtnNav.addEventListener('click', () =>{
+        modal(j)
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () =>{
+        modalViews.forEach((modalView) =>{
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
+
+/*====================  GALLERY SWIPER ====================*/
+let swiperPortfolio = new Swiper(".gallery__container", {
+    cssMode: true,
+    loop: true,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+/*==================== POPUP ====================*/
 const btnOpenVideo = document.querySelectorAll('.features__video-content')
 const featuresPopup = document.getElementById('popup')
 
 function poPup(){
     featuresPopup.classList.add('show-popup')
 }
-
 btnOpenVideo.forEach(b => b.addEventListener('click', poPup))
 
 const btnCloseVideo = document.getElementById('popup-close')
@@ -44,15 +89,16 @@ btnCloseVideo.addEventListener('click', ()=> {
     featuresPopup.classList.remove('show-popup')
 })
 
-/*==================== GSAP ANIMATION ==================*/
+/*==================== GSAP ANIMATION ====================*/
 const controlImg = document.querySelectorAll('.controls__img')
 
-function ScrollAnimation(){
-    gsap.from('.features__subtitle', {opacity: 0, duration: .2, delay: .2, y:-20})
-    gsap.from('.features__title', {opacity: 0, duration: .3, delay: .3, y:-20})
-    gsap.from('.features__description', {opacity: 0, duration: .4, delay: .4, y:-20})
-    gsap.from('.features__button', {opacity: 0, duration: .2, delay: .5, y:-20})
-    gsap.from('.features__video-content', {opacity: 0, duration: .6, delay: .2, y:-20})
+function scrollAnimation(){
+    gsap.from('.features__subtitle', {opacity: 0, duration: .2, delay: .2, y: -20})
+    gsap.from('.features__title', {opacity: 0, duration: .3, delay: .3, y: -20})
+    gsap.from('.features__description', {opacity: 0, duration: .4, delay: .4, y: -20})
+    gsap.from('.features__description-two', {opacity: 0, duration: .4, delay: .4, y: -20})
+    gsap.from('.features__button', {opacity: 0, duration: .5, delay: .5, y: -20})
+    gsap.from('.features__video-content', {opacity: 0, duration: .6, delay: .6, y: -20})
 
     featuresPopup.classList.remove('show-popup')
 }
